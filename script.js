7f1bb4a4150f2d6d8826dbfa03f8b45a490fa752
myungho <skqoaudgh2@hanmail.net>
2019-07-09 16:32:30 +0900

    캔버스를 이용한 원형 타이머 모양및 중앙 시간 텍스트 출력 작성

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let times = document.getElementsByClassName('time');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
 // util Function
 function checkTimeInput(event) {
@@ -13,6 +15,35 @@ function checkTimeInput(event) {
     }
 }
 
+// canvas Function
+function initialCanvas() {
+    ctx.beginPath();
+    ctx.arc(300, 300, 250, 0, Math.PI*2);
+    ctx.fillStyle = '#e6e6e6';
+    ctx.fill();
+}
+
+function drawPieTimer(curTime, endTime) {
+    let angle = -Math.PI/2 + (Math.PI/180)*(curTime/endTime)*360;
+    ctx.beginPath();
+    ctx.arc(300, 300, 240, -Math.PI/2, angle);
+    ctx.strokeStyle = '#ff7474';
+    ctx.lineWidth = 20;
+    ctx.stroke(); 
+}
+
+function drawTimeText(text) {
+    ctx.beginPath();
+    ctx.font = "60px Helvetica";
+    ctx.fillStyle = "grey";
+    ctx.textAlign = "center";
+    ctx.fillText(text, 300, 300);
+}
+
+// eventListener Function
 for(let i=0; i<3; i++) {
     times[i].addEventListener('change', checkTimeInput);
 }
+
+//-----
+initialCanvas();
