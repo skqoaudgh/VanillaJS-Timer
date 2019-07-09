aa73f0d3decf6457744a0517c6a438a143bd60ea
myungho <skqoaudgh2@hanmail.net>
2019-07-09 17:44:55 +0900

    중지 기능 추가. 타이머 중앙 시간 텍스트 출력 기능 추가

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let pause = document.getElementById('pause');
 let reset = document.getElementById('reset');
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
+let timetext = document.getElementById('timetext');
+
 let paused = false;
 let isActive = false;
 let timer;
@@ -23,18 +25,41 @@ function checkTimeInput(event) {
     }
 }
 
+function resetTimerDrawing() {
+    ctx.beginPath();
+    ctx.arc(300, 300, 240, -Math.PI/2, Math.PI*2);
+    ctx.strokeStyle = '#e6e6e6';
+    ctx.lineWidth = 22;
+    ctx.stroke(); 
+}
+
+function secondsToTimeString(sec) {
+    let hours = Math.floor(sec / 3600);
+    let minutes = Math.floor((sec - (hours*3600)) / 60);
+    let seconds = sec - (hours*3600) - (minutes*60);
+
+    if(hours < 10) {
+        hours = "0" + hours;
+    }
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    return hours+"h "+minutes+"m "+seconds+"s"
+}
+
 function resetTimer() {
     if(isActive) {
-        ctx.beginPath();
-        ctx.arc(300, 300, 240, -Math.PI/2, Math.PI*2);
-        ctx.strokeStyle = '#e6e6e6';
-        ctx.lineWidth = 22;
-        ctx.stroke(); 
-    
+        resetTimerDrawing();
+
         paused = false;
+        isActive = false;
         clearInterval(timer);
         total = 0;
         cur = 0;
+        timetext.innerHTML = '';
         for(let i=0; i<3; i++) {
             times[i].value = '';
         }
@@ -42,23 +67,35 @@ function resetTimer() {
 }
 
 function startTimer() {
-    if(!paused) {
-        total = (+times[0].value)*3600 + (+times[1].value)*60 + (+times[2].value);
-        if(total > 0) {
-            cur = 0;
+    if(!isActive) {
+        if(!paused) {
+            resetTimerDrawing();
+            total = (+times[0].value)*3600 + (+times[1].value)*60 + (+times[2].value);
+            if(total > 0) {
+                cur = 0;
+                timer = setInterval(function() {
+                    cur += 0.02;
+                    timetext.innerHTML = secondsToTimeString(Math.floor(total-cur)+1);
+                    drawPieTimer(cur, total);
+                    if(cur >= total) {
+                        clearInterval(timer);
+                    }
+                }, 20);
+                isActive = true;
+            }        
+        }
+        else {
             timer = setInterval(function() {
                 cur += 0.02;
+                timetext.innerHTML = secondsToTimeString(Math.floor(total-cur)+1);
                 drawPieTimer(cur, total);
+                if(cur >= total) {
+                    clearInterval(timer);
+                }
             }, 20);
+            paused = false;     
             isActive = true;
         }
-    }
-    else {
-        timer = setInterval(function() {
-            cur += 0.02;
-            drawPieTimer(cur, total);
-        }, 20);
-        isActive = true;
     }
 }
 
@@ -67,6 +104,9 @@ function pauseTimer() {
         paused = true;
         isActive = false;
         clearInterval(timer);
+    }
+    else {
+        startTimer();
     }
 }
 
@@ -85,14 +125,6 @@ function drawPieTimer(curTime, endTime) {
     ctx.strokeStyle = '#ff7474';
     ctx.lineWidth = 20;
     ctx.stroke(); 
-}
-
-function drawTimeText(text) {
-    ctx.beginPath();
-    ctx.font = "60px Helvetica";
-    ctx.fillStyle = "grey";
-    ctx.textAlign = "center";
-    ctx.fillText(text, 300, 300);
 }
 
 // eventListener Function
